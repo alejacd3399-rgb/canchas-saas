9be503d628c9c1e716fa6ba95ce3b1868272b77a
Claudia <claucdcc@gmail.com>
2026-06-08 19:10:30 -0500

    Agregar publishable client key

--- lib/stack.ts
+++ lib/stack.ts
@@ -2,7 +2,6 @@ import { StackServerApp } from "@stackframe/stack";
 
 export const stackServerApp = new StackServerApp({
   tokenStore: "nextjs-cookie",
-  publishableClientKey: "2f4517c0-9899-4a10-a874-ea4ea5b5d9de",
   urls: {
     afterSignIn:  "/dashboard",
     afterSignUp:  "/dashboard",
